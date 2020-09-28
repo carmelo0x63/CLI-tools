d8ff91af99db98d54157cfa410c9f76928384697
Carmelo C <carmelo.califano@gmail.com>
2020-09-28 12:24:52 +0200

    Python/netscan: improved argument parsing

--- Python/netscan.py
+++ Python/netscan.py
@@ -3,29 +3,41 @@
 # author: Carmelo C
 # email: carmelo.califano@gmail.com
 # history:
-#  1.0 First version
+#  1.1: Small improvement to argument parsing
+#  1.0: First version
 
 import argparse
+import ipaddress
 import socket
 import sys
 
 # Version number
-__version__ = "1.0"
-__build__ = "20200520"
+__version__ = "1.1"
+__build__ = "20200928"
 
 def main():
     parser = argparse.ArgumentParser(description='Network scanner looking for open ports, version ' + __version__ + ', build ' + __build__ + '.')
-    parser.add_argument('subnet', metavar='<subnet>', type=str, help='Subnet either in CIDR (e.g. 192.168.1.0/24) or subnet mask (e.g. 192.168.1.0/255.255.255.0) format')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+#    parser.add_argument('subnet', metavar='<subnet>', type=str, help='Subnet either in CIDR (e.g. 192.168.1.0/24) or subnet mask (e.g. 192.168.1.0/255.255.255.0) format')
+    parser.add_argument('host', metavar='<host>', type=str, help='Host / literal IP address')
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + __version__)
 
     # Set the default timeout in seconds (float) for new socket objects
     socket.setdefaulttimeout(1)
 
-    # Check the argument list, in case of no args we quit
+    # In case of no arguments shows help message
     if len(sys.argv) == 1:
-        sys.exit(1)
+        parser.print_help()
+        sys.exit(10)  # ERROR: no arguments
+    else:
+        args = parser.parse_args() # parse command line
 
     target = sys.argv[1]
+    try:
+        ipaddress.ip_address(target)
+    except ValueError:
+        print('Error: ' + str(e))
+        sys.exit(20)  # ERROR: input is not an IP address
+
     for port in [22, 80, 123, 443, 8080]:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         conn = s.connect_ex((target, port))
