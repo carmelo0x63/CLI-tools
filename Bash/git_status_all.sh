b33f7ad702a3a2c38887d7b629ac0e789e13e9c0
Carmelo C <carmelo.califano@gmail.com>
2022-01-12 16:24:59 +0100

    Refactored git_status and git_fetch for ZSH

--- Bash/git_status_all.sh
+++ Bash/git_status_all.sh
@@ -1,26 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 # Iterates through the local repositories, assumed to be under TARGETDIR
 # author: Carmelo C
 # email: carmelo.califano@gmail.com
 # history, date format ISO 8601:
+#  2022-01-12: Refactored for ZSH
 #  2020-04-06: First release
 
 # Settings
-TARGETDIR="$HOME/github"
 SEPARATOR="==============================================="
 
 echo "[+] Checking your GIT repositories..."
 echo
 
-for Repo in $(ls "$TARGETDIR"); do
-        cd "$TARGETDIR"/"$Repo"
+for Repo in ./*(/)
+    (
         echo "$SEPARATOR"
-        echo "$PWD"
+        echo "$Repo..."
+        cd $Repo
         echo "$SEPARATOR"
         git status -s
-        echo "$SEPARATOR"
-        echo -e "\n"
-done
-
+        echo -e "$SEPARATOR\n\n"
+    )
 echo -e "[+] Done!\n"
 
