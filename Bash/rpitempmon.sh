3f3c216ae9acd624c8515b9a31ea1637abc099b2
Carmelo C <carmelo.califano@gmail.com>
2023-07-19 10:41:54 +0200

    Edited Bash/rpitempmon

--- Bash/rpitempmon.sh
+++ Bash/rpitempmon.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Temperature monitoring on Raspberry Pi"
+echo "[+] Temperature monitoring on Raspberry Pi"
 
 while true
-    do sleep 5 && sudo /opt/vc/bin/vcgencmd measure_temp
+    do sleep 5 && /usr/bin/vcgencmd measure_temp
 done
 
